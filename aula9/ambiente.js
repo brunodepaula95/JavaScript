b0c145ac14bb4e7a4102c80e245420a5f3532db6
Bruno de Paula <brunohpaula1995@gmail.com>
2024-08-24 17:16:13 -0300

    Repetições pt2

---
+++ aula9/ambiente.js
@@ -0,0 +1,6 @@
+//for (inicio;teste; incremento){
+//} ele começa pela inicialização, e faz os testes, voltando para o inicio e incrementando, quando o teste lógico for falso, ele sai do bloco
+
+for(var c = 1; c <= 10; c++){
+    console.log(c)
+}
