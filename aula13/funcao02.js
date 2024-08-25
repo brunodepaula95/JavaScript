36b90a001f91f67de72166f56e9f89e9589d1587
Bruno de Paula <brunohpaula1995@gmail.com>
2024-08-24 23:18:12 -0300

    Funções

---
+++ aula13/funcao02.js
@@ -0,0 +1,5 @@
+function soma(n1=0, n2=0){//esse parametro igual a zero serve para quando você solicitar dois valores, mas inserir apenas um, ele preenche com 0
+    return n1 + n2
+}
+
+console.log(soma(7))
---
+++ aula13/funcao03.js
@@ -0,0 +1,5 @@
+let v = function(x){ //passar uma função dentro de uma variavel
+    return x*2
+}
+
+console.log(v(5))
