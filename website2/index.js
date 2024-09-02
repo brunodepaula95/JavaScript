4869797913cc2530e71767fffc994d325be823fd
Bruno de Paula <brunohpaula1995@gmail.com>
2024-09-02 16:51:54 -0300

    constantes

--- website2/index.js
+++ website2/index.js
@@ -1,6 +1,6 @@
 //type conversion = change the datatype of a value to another(string, numbers, booleans)
 
-let x = "pizza";
+/*let x = "pizza";
 let y = "pizza";
 let z = "pizza";
 
@@ -10,4 +10,20 @@ z = Boolean(z);
 
 console.log(x, typeof x);
 console.log(y, typeof y);
-console.log(z, typeof z);
+console.log(z, typeof z);*/
+
+
+//const = a variable that can't be changed
+
+const PI = 3.14159;
+let radius;
+let circumference;
+
+
+
+document.getElementById("mySubmit").onclick = function(){
+  radius = document.getElementById("myText").value;
+  radius = Number(radius);
+  circumference = 2 * PI * radius;
+  document.getElementById("myH3").textContent = circumference;
+}
