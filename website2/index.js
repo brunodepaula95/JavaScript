81a7d0e5d98a0ab3f285723262aa6f6b3625c4c9
Bruno de Paula <brunohpaula1995@gmail.com>
2024-09-02 17:58:39 -0300

    Progrma simples de números e metodo Math

--- website2/index.js
+++ website2/index.js
@@ -15,7 +15,7 @@ console.log(z, typeof z);*/
 
 //const = a variable that can't be changed
 
-const PI = 3.14159;
+/*const PI = 3.14159;
 let radius;
 let circumference;
 
@@ -26,4 +26,27 @@ document.getElementById("mySubmit").onclick = function(){
   radius = Number(radius);
   circumference = 2 * PI * radius;
   document.getElementById("myH3").textContent = circumference;
-}
+}*/
+
+//Math = built-in object that provides a collection of properties and methods
+
+let x = 3.21;
+let y = 2;
+let z;
+
+z = Math.round(x);//usa esse metodo para arredondar um numero.
+z = Math.floor(x);//arredonda para baixo
+z = Math.ceil(x); //arredonda para cima
+z = Math.trunc(x); //elimina depois do ponto
+z = Math.pow(x, y);//eleva x a y
+z = Math.sqrt(x);//enconta a raiz quadrada
+z = Math.log(x);//enconta o logaritimo
+z = Math.cos(x);//enconta o cosseno
+z = Math.abs(x);//te da o valor absoluto mas positivo
+let max = Math.max(x, y, z); //encontra o valor maximo dentre os 3
+let min = Math.min(x, y, z); //encontra o valor minimo dentre os 3
+
+
+
+
+
