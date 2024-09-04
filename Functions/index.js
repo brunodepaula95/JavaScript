84213b967255983d226f5e19e70a63f5dfde5c15
Bruno de Paula <brunohpaula1995@gmail.com>
2024-09-04 18:51:31 -0300

    Programa para converter temperaturas

---
+++ Functions/index.js
@@ -0,0 +1,46 @@
+//function = A section of reusable code. Declare code once, use it whenever you want. Call the function to execute that code
+
+/*function happyBirthday(username, age){//passe parametros dentro dos parenteses
+  console.log("Happy birthday to you!");
+  console.log(`Happy birthday dear ${username}!`);
+  console.log(`You are ${age} years old`);
+}
+
+happyBirthday("Bruno", 29); //chama a função para imprimir na tela, dentro de paranteses depois de chamar a função se chama "argumentos", mas para que eles funcionem, você precisa primeiro passar parametros na função
+
+*/
+
+
+
+/*function add(x, y){
+    //let result = x + y;//você não precisa colocar uma variavel dentro da função
+    return x + y;//basta inserir já no retorno o calculo que deja fazer
+}
+
+console.log(add(2, 3));
+*/
+
+//Função para ver se numero é positivo
+//function isEven(number){
+//    return number % 2 === 0? true : false;
+//}
+
+/*
+//Função para ver se o emai é valido com if
+function isValidEmail(email){
+      if(email.includes("@")){
+        return true;
+      }
+      else{
+        return false;
+      }
+}
+
+*/
+
+/*
+Função para ver se email é valido com ternario
+function isValidEmail(email){
+     return email.includes("@")? true : false;
+}
+*/
